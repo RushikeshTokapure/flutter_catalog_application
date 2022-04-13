a43db9f7664a73a9af8971d4f39158a5f28958c7
Rushikesh Shivraj Tokapure <rushitokapure@gmail.com>
2022-04-13 19:10:35 +0530

    task2 commit

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int i = 2;
+  final String name = "Rushikesh";
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog"),
+      ),
+      body: Center(
+          child: Container(
+        child: Text("Welcome $name"),
+      )),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // int i = 2;
+  // String name = "Rushikesh";
+  // double pi = 3.14;
+  // bool isTrue = true;
+  // num temp = 20.9;
+  // var day = "Wednesday";
+  // static const pie = 3.142;
+  // final p = 3.1;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-            child: Container(
-          child: Text("Welcome"),
-        )),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
