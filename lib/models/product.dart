601fbf2dc1e3a8f62b0f3f421d4fc1bc9cf988ce
Rushikesh Shivraj Tokapure <rushitokapure@gmail.com>
2022-04-16 19:09:18 +0530

    task9 commit

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final products = [
+    Product(
+        id: 1,
+        name: "Android",
+        desc: "Mobile",
+        price: 20000,
+        color: "#33505a",
+        image:
+            "https://avatars.githubusercontent.com/u/55596957?s=400&u=18e04de64334c6fa3149801e0419f360ef11a239&v=4https://avatars.githubusercontent.com/u/55596957?s=400&u=18e04de64334c6fa3149801e0419f360ef11a239&v=4")
+  ];
+}
+
 class Product {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -14,13 +27,3 @@ class Product {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Product(
-      id: "001",
-      name: "Android",
-      desc: "Mobile",
-      price: 20000,
-      color: "#33505a",
-      image: "https://images.app.goo.gl/BBfKYTN8JaasVMbm7")
-];
